3c409f37ec76aa95f21e3e16b2a5954c2e7fabe7
Michał Płużek <michalpluzek@gmail.com>
2021-12-20 14:38:39 +0100

    feat: add choice of info type

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 import fetch from "node-fetch";
 
-const year = process.argv[2] || Math.floor(Math.random() * 2022);
+const arg = process.argv[2];
+let type = "";
 
-fetch(`http://numbersapi.com/${year}/year?json`)
+if (arg.indexOf("--year") === 0) {
+  console.log("szukam informacji o roku...");
+  type = "year";
+} else if (arg.indexOf("--math") === 0) {
+  console.log('"szukam informacji o liczbie...');
+  type = "math";
+} else if (arg.indexOf("--trivia") === 0) {
+  console.log('"szukam liczby-ciekawostki...');
+  type = "trivia";
+}
+
+const equalSign = arg.search("=");
+
+if (equalSign === -1) console.log("Nie wpisałeś liczby!");
+
+const number = arg.slice(equalSign + 1);
+
+if (number === "" || isNaN(Number(number))) {
+  console.log("To nie jest liczba!");
+  process.exit();
+}
+
+fetch(`http://numbersapi.com/${number}/${type}?json`)
   .then((response) => {
-    console.log(response.status);
-    console.log(response.ok);
-    return response.json();
+    if (response.ok) {
+      return response.json();
+    } else throw new Error("Coś poszło nie tak. Status: " + response.status);
   })
-  .then((data) => console.log(data))
+  .then((data) => console.log(data.text))
   .catch((err) => console.log("Error: ", err));
---
+++ readme.txt
@@ -0,0 +1,4 @@
+to start write in terminal: 
+- info about year np. node app.js --year=2020,
+- info about number np. node app.js --math=2,
+- info about trivia np. node app.js --trivia=111,
